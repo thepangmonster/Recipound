7e223dbbd9d15966c420ecd2f06368a18cf20684
Brandon <pangilinab@gmail.com>
2020-08-30 19:49:12 -0700

    add browser router to top level module so that the application can support more than 1 frontend page

--- recipound/src/App.tsx
+++ recipound/src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 import { EditRecipePage } from './frontend/EditRecipePage';
+import { BrowserRouter as Router, Route } from 'react-router-dom'; 
 
 function App() {
   return (
-    <div>
-      <EditRecipePage/> 
-    </div>
+    <Router>
+        <>
+          <Route path="/" component={EditRecipePage}/>
+          <Route path="edit-recipe" component={EditRecipePage}/>
+        </>
+    </Router>
+
   );
 }
 
