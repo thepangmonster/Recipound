da5d15e4e75bf4c099c72b766cfd213fa2df877a
Brandon <pangilinab@gmail.com>
2020-09-04 17:06:05 -0700

    remove formik render deprecation from the add ingredient form

--- recipound/src/frontend/AddIngredientForm.tsx
+++ recipound/src/frontend/AddIngredientForm.tsx
@@ -1,47 +1,55 @@
 import React, { Component } from "react";
 import { Formik, Form, Field } from "formik"; 
 import { TextField } from "formik-material-ui";
-import { Button } from '@material-ui/core'
+import { Button, Paper } from '@material-ui/core'
 
 export class AddIngredientForm extends Component {
 
     handleSubmit = (values: any, {
-        setSubmitting
+        setSubmitting,
     }: any) => {
         setSubmitting(false)
+        console.log(values)
         return
+        
+    }
+
+    initialValues = {
+        rawIngredient: '',
+        quantity: '',
+        unit: '',
+        packageAmount: '',
+        packageUnit: '',
+        packageCost: '', 
+        source: '',
     }
 
     render () {
         return (
             <Formik
-            initialValues = {{
-                rawIngredient: '',
-                quantity: '',
-                unit: '',
-                packageAmount: '',
-                packageUnit: '',
-                packageCost: '', 
-                source: '',
-            }}
-            onSubmit = {this.handleSubmit}
-            render = { formProps => { 
+            initialValues = {this.initialValues}
+            onSubmit = {this.handleSubmit}>
+            {formProps => { 
                 return(
-                    <Form>
-                        <Field type="text" name="rawIngredient" placeholder="Raw Ingredient" component={ TextField }/>
-                        <Field type="number" name="quantity" placeholder="Quantity" component={ TextField }/>
-                        <Field type="text" name="unit" placeholder="Unit" component={ TextField }/>
-                        <Field type="number" name="packageAmount" placeholder="Package Amount" component={ TextField }/>
-                        <Field type="text" name="packageUnit" placeholder="Package Unit" component={ TextField }/>
-                        <Field type="number" name="packageCost" placeholder="Package Cost" component={ TextField }/>
-                        <Field type="text" name="source" placeholder="Source" component={ TextField }/>
-                        <Button type="submit"
-                                disabled={formProps.isSubmitting}>
-                                Add to Recipe
-                        </Button>
-                    </Form>
+                    <Paper>
+                        <Form>
+                            <Field type="text" name="rawIngredient" placeholder="Raw Ingredient" component={TextField}/>
+                            <Field type="number" name="quantity" placeholder="Quantity" component={TextField}/>
+                            <Field type="text" name="unit" placeholder="Unit" component={TextField}/>
+                            <Field type="number" name="packageAmount" placeholder="Package Amount" component={TextField}/>
+                            <Field type="text" name="packageUnit" placeholder="Package Unit" component={TextField}/>
+                            <Field type="number" name="packageCost" placeholder="Package Cost" component={TextField}/>
+                            <Field type="text" name="source" placeholder="Source" component={TextField}/>
+                            <Button type="submit"
+                                    disabled={formProps.isSubmitting}
+                                    variant="contained">
+                                    Add to Recipe
+                            </Button>
+                        </Form>
+                    </Paper>
                 ); 
             }}
-        />);
+            </Formik>
+        );
     }
 }
