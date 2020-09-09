87a8185ba994c7f51238953bdcb9d3210b33de58
Brandon <pangilinab@gmail.com>
2020-09-08 21:08:27 -0700

    Add resource entities for the backend

---
+++ recipound/src/backend/src/main/resources/Ingredient.kt
@@ -0,0 +1,11 @@
+//Retrieve all ingredients of a recipe
+GET /recipes/{recipe}/ingredients
+
+//Delete an ingredient of a recipe
+DELETE /recipes/{recipe}/ingredients/{ingredient_id}
+
+//Edit an ingredient of a recipe
+PUT /recipes/{recipe}/ingredients/{ingredient_id}
+
+//Create a new ingredient of a recipe
+POST /recipes/{recipe}/ingredients/
---
+++ recipound/src/backend/src/main/resources/Recipe.kt
@@ -0,0 +1,7 @@
+//Retrieve all recipes of a user
+
+//Delete a recipe of a user
+
+//Edit a recipe of a user 
+
+//Add a recipe of a user
